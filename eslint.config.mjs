36679b3092830adfb0dee8d1fddb4820928abaf0
nayounsang <nayounsang722@gmail.com>
2025-08-30 16:30:43 +0900

    chore: setup vitest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,36 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import vitest from "@vitest/eslint-plugin";
 
-export default tseslint.config(eslint.configs.recommended, {
-  files: ["src/**/*.ts", "src/**/*.tsx"],
-  extends: [
-    eslint.configs.recommended,
-    tseslint.configs.recommendedTypeChecked,
-  ],
-  languageOptions: {
-    parserOptions: {
-      projectService: true,
-      tsconfigRootDir: import.meta.dirname,
+export default tseslint.config([
+  {
+    files: ["src/**/*.ts", "src/**/*.tsx"],
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
+    ],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
-});
+  {
+    files: ["tests/**/*.{test,spec}.ts"],
+    extends: [eslint.configs.recommended, tseslint.configs.recommended],
+    plugins: {
+      vitest,
+    },
+    rules: {
+      ...vitest.configs.recommended.rules,
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+]);
